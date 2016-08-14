702afec53566b964bbabb0e1bb668b712d31d21c
leminhtu1204 <leminhtu12041988@gmail.com>
2016-08-14 21:39:55 +0700

    Tu

---
+++ ngQuiz/ngQuiz/app/app.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+var app = angular.module('quizApp', ['ngRoute', 'ngSanitize'])
+// Routing has been added to keep flexibility in mind. This will be used in future.
+angular.module('quizApp')
+.config(['$routeProvider',
+  function ($routeProvider) {
+
+      var routes = [
+          {
+              url: '/home',
+              template: 'templates/quizControl/quiz.html',
+              controller: 'quizCtrl'
+          },
+      ];
+
+      routes.forEach(function (r, index) {
+          $routeProvider.when(r.url, { templateUrl: r.template, controller: r.controller });
+      });
+
+      $routeProvider.otherwise({ redirectTo: '/home' });
+  }]);
---
+++ ngQuiz/ngQuiz/templates/quizControl/quizCtrl.js
@@ -0,0 +1,105 @@
+﻿var quizCtrl = function ($scope, $http, helper) {
+    $scope.quizName = 'data/csharp.js';
+
+    $scope.defaultConfig = {
+        'allowBack': true,
+        'allowReview': true,
+        'autoMove': false,  // if true, it will move to next question automatically when answered.
+        'duration': 0,  // indicates the time in which quiz needs to be completed. post that, quiz will be automatically submitted. 0 means unlimited.
+        'pageSize': 1,
+        'requiredAll': false,  // indicates if you must answer all the questions before submitting.
+        'richText': false,
+        'shuffleQuestions': false,
+        'shuffleOptions': false,
+        'showClock': false,
+        'showPager': true,
+        'theme': 'none'
+    }
+
+    $scope.goTo = function (index) {
+        if (index > 0 && index <= $scope.totalItems) {
+            $scope.currentPage = index;
+            $scope.mode = 'quiz';
+        }
+    }
+
+    $scope.onSelect = function (question, option) {
+        if (question.QuestionTypeId == 1) {
+            question.Options.forEach(function (element, index, array) {
+                if (element.Id != option.Id) {
+                    element.Selected = false;
+                }
+            });
+        }
+
+        if ($scope.config.autoMove == true && $scope.currentPage < $scope.totalItems)
+            $scope.currentPage++;
+    }
+
+    $scope.onSubmit = function () {
+        var answers = [];
+        $scope.questions.forEach(function (q, index) {
+            answers.push({ 'QuizId': $scope.quiz.Id, 'QuestionId': q.Id, 'Answered': q.Answered });
+        });
+        $scope.mode = 'result';
+    }
+
+    $scope.pageCount = function () {
+        return Math.ceil($scope.questions.length / $scope.itemsPerPage);
+    };
+
+    $scope.loadQuiz = function (file) {
+        $http.get(file)
+         .then(function (res) {
+             $scope.quiz = res.data.quiz;
+             $scope.config = helper.extend({}, $scope.defaultConfig, res.data.config);
+             $scope.questions = $scope.config.shuffleQuestions ? helper.shuffle(res.data.questions) : res.data.questions;
+             $scope.totalItems = $scope.questions.length;
+             $scope.itemsPerPage = $scope.config.pageSize;
+             $scope.currentPage = 1;
+             $scope.mode = 'quiz';
+             if($scope.config.shuffleOptions)
+                $scope.shuffleOptions();
+
+             $scope.$watch('currentPage + itemsPerPage', function () {
+                 var begin = (($scope.currentPage - 1) * $scope.itemsPerPage),
+                   end = begin + $scope.itemsPerPage;
+
+                 $scope.filteredQuestions = $scope.questions.slice(begin, end);
+             });
+         });
+    }
+    
+    $scope.shuffleOptions = function(){
+        $scope.questions.forEach(function (question) {
+           question.Options = helper.shuffle(question.Options);
+        });
+    }
+    
+    $scope.loadQuiz($scope.quizName);
+
+    $scope.isAnswered = function (index) {
+        var answered = 'Not Answered';
+        $scope.questions[index].Options.forEach(function (element, index, array) {
+            if (element.Selected == true) {
+                answered = 'Answered';
+                return false;
+            }
+        });
+        return answered;
+    };
+
+    $scope.isCorrect = function (question) {
+        var result = 'correct';
+        question.Options.forEach(function (option, index, array) {
+            if (helper.toBool(option.Selected) != option.IsAnswer) {
+                result = 'wrong';
+                return false;
+            }
+        });
+        return result;
+    };
+}
+
+quizCtrl.$inject = ['$scope', '$http', 'helperService'];
+app.controller('quizCtrl', quizCtrl);
